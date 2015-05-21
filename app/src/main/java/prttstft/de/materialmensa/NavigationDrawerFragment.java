d32906bab7ea9a1baf0a6fd2607716bb9c4b7bf8
prttstft <prttstft@gmail.com>
2015-05-21 02:08:03 +0200

    Working on RecyclerView.

--- app/src/main/java/prttstft/de/materialmensa/NavigationDrawerFragment.java
+++ app/src/main/java/prttstft/de/materialmensa/NavigationDrawerFragment.java
@@ -7,11 +7,15 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -25,6 +29,8 @@ public class NavigationDrawerFragment extends android.support.v4.app.Fragment {
 
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
+
+    private data_row_navdraw_adapter adapter;
 
     private boolean mUserLearnedDrawer;
     private boolean mFromSavedInstanceState;
@@ -50,9 +56,24 @@ public class NavigationDrawerFragment extends android.support.v4.app.Fragment {
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.fragment_navigation_drawer, container, false);
         recyclerView = (RecyclerView) layout.findViewById(R.id.drawerList);
+        adapter = new data_row_navdraw_adapter(getActivity(), getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         return layout;
     }
 
+    public static List<data_row_navdraw> getData(){
+        List<data_row_navdraw> data = new ArrayList<>();
+        int[] icons = {R.drawable.ic_ndrawer_icon1, R.drawable.ic_ndrawer_icon2, R.drawable.ic_ndrawer_icon3, R.drawable.ic_ndrawer_icon4};
+        String[] titles={"Item 1","Item 2", "Item 3", "Item 4"};
+        for(int i=0;i<titles.length && i< icons.length;i++) {
+            data_row_navdraw current = new data_row_navdraw();
+            current.iconId = icons[i];
+            current.title = titles[i];
+            data.add(current);
+        }
+        return data;
+    }
 
     public void setUp(int fragmentId,DrawerLayout drawerLayout, Toolbar toolbar) {
         containerView = getActivity().findViewById(fragmentId);
--- app/src/main/java/prttstft/de/materialmensa/data_row_navdraw_adapter.java
+++ app/src/main/java/prttstft/de/materialmensa/data_row_navdraw_adapter.java
@@ -8,32 +8,40 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Max on 21.05.2015.
  */
 public class data_row_navdraw_adapter extends RecyclerView.Adapter<data_row_navdraw_adapter.MyViewHolder> {
 
     private final LayoutInflater infalter;
+    List<data_row_navdraw> data = Collections.emptyList();
 
-    public data_row_navdraw_adapter(Context context) {
+    public data_row_navdraw_adapter(Context context, List<data_row_navdraw> data) {
         infalter = LayoutInflater.from(context);
+        this.data = data;
     }
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = infalter.inflate(R.layout.custom_row, parent, false);
         MyViewHolder holder = new MyViewHolder(view);
-        return null;
+        return holder;
     }
 
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int i) {
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        data_row_navdraw current = data.get(position);
+        holder.title.setText(current.title);
+        holder.icon.setImageResource(current.iconId);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
