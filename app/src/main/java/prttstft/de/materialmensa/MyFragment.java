fa5e6ebd06da5149e51e27479608dbf7364733e6
prttstft <prttstft@gmail.com>
2015-05-22 20:47:31 +0200

    Tabs commin' yo!

--- app/src/main/java/prttstft/de/materialmensa/MyFragment.java
+++ app/src/main/java/prttstft/de/materialmensa/MyFragment.java
@@ -17,8 +17,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import prttstft.de.materialmensa.network.VolleySingleton;
+
 public class MyFragment extends Fragment {
     private TextView textView;
+
     public static MyFragment getInstance(int position) {
         MyFragment myFragment = new MyFragment();
         Bundle args = new Bundle();
@@ -27,7 +30,7 @@ public class MyFragment extends Fragment {
 
         return myFragment;
     }
-//
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstaceState) {
         View layout = inflater.inflate(R.layout.fragment_my, container, false);
@@ -37,18 +40,19 @@ public class MyFragment extends Fragment {
             textView.setText("The Page Selected is " + bundle.getInt("position"));
         }
 
-        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-        StringRequest request = new StringRequest(Request.Method.GET, "http://www.php.net", new Response.Listener<String>() {
+        RequestQueue requestQueue = VolleySingleton.getInstance().getRequestQueue();
+        StringRequest request = new StringRequest(Request.Method.GET, "http://jsonplaceholder.typicode.com/posts", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                Toast.makeText(getActivity(),"RESPONSE " + response, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "RESPONSE " + response, Toast.LENGTH_SHORT).show();
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getActivity(),"ERROR " + error, Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "ERROR " + error, Toast.LENGTH_SHORT).show();
             }
         });
+        requestQueue.add(request);
         return layout;
     }
 }
