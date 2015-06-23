103cd3116113cedf116e9637ec0aa996cd014004
prttstft <prttstft@gmail.com>
2015-06-23 23:52:14 +0200

    Cleaning up.

--- app/src/main/java/de/prttstft/materialmensa/extras/Keys.java
+++ app/src/main/java/de/prttstft/materialmensa/extras/Keys.java
@@ -1,15 +1,15 @@
 package de.prttstft.materialmensa.extras;
 
 public interface Keys {
-    public interface EndpointToday {
-        public static final String KEY_NAME_DE = "name_de";
-        public static final String KEY_NAME_EN = "name_en";
-        public static final String KEY_CATEGORY = "category";
-        public static final String KEY_TARA = "pricetype";
-        public static final String KEY_STUDENTS = "priceStudents";
-        public static final String KEY_STAFF = "priceWorkers";
-        public static final String KEY_GUESTS = "priceGuests";
-        public static final String KEY_ALLERGENS = "allergens";
-        public static final String KEY_BADGE = "badges";
+    interface EndpointToday {
+        String KEY_NAME_DE = "name_de";
+        String KEY_NAME_EN = "name_en";
+        String KEY_CATEGORY = "category";
+        String KEY_TARA = "pricetype";
+        String KEY_STUDENTS = "priceStudents";
+        String KEY_STAFF = "priceWorkers";
+        String KEY_GUESTS = "priceGuests";
+        String KEY_ALLERGENS = "allergens";
+        String KEY_BADGE = "badges";
     }
 }
