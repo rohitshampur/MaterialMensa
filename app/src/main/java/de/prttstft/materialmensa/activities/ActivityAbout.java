539a2f9930ecb8ba34c47c055d8ab0dca98f0067
prttstft <prttstft@gmail.com>
2015-06-24 02:56:52 +0200

    Cleanup & About.

--- app/src/main/java/de/prttstft/materialmensa/activities/ActivityAbout.java
+++ app/src/main/java/de/prttstft/materialmensa/activities/ActivityAbout.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -21,7 +23,6 @@ import de.prttstft.materialmensa.logging.L;
 
 
 public class ActivityAbout extends AppCompatActivity {
-    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +30,7 @@ public class ActivityAbout extends AppCompatActivity {
         setContentView(R.layout.activity_about);
 
         // Adding the Toolbar
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         if (Build.VERSION.SDK_INT >= 21) {
             toolbar.setElevation(4);
         }
@@ -39,17 +40,32 @@ public class ActivityAbout extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
         Calendar c = Calendar.getInstance();
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         String today = df.format(c.getTime());
-
 
         Calendar d = Calendar.getInstance();
         d.add(Calendar.DAY_OF_YEAR, 1);
         SimpleDateFormat dfd = new SimpleDateFormat("yyyy-MM-dd");
         String tomorrow = dfd.format(d.getTime());
 
+        TextView about_about = (TextView) findViewById(R.id.about_about);
+        TextView about_aboutText = (TextView) findViewById(R.id.about_aboutText);
 
+        TextView icons = (TextView) findViewById(R.id.about_icons);
+        TextView iconsText = (TextView) findViewById(R.id.about_iconsText);
+
+        TextView libraries = (TextView) findViewById(R.id.about_libraries);
+        TextView librariesText = (TextView) findViewById(R.id.about_librariesText);
+
+        TextView api = (TextView) findViewById(R.id.about_api);
+        TextView apiText = (TextView) findViewById(R.id.about_apiText);
+
+        about_aboutText.setMovementMethod(LinkMovementMethod.getInstance());
+        librariesText.setMovementMethod(LinkMovementMethod.getInstance());
+        iconsText.setMovementMethod(LinkMovementMethod.getInstance());
+        apiText.setMovementMethod(LinkMovementMethod.getInstance());
 }
 
 
@@ -58,29 +74,7 @@ public class ActivityAbout extends AppCompatActivity {
     // Options
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_about, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            Intent i = new Intent(this, ActivityPreferences.class);
-            startActivity(i);
-            return true;
-        }
-
-        if (id == android.R.id.home) {
-            finish();
-        }
-
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 
 }
